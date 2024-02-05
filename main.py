dad02bd93356a4563c648c3b5ba28bf77380d523
berserkxr <stefanzizov@gmai.com>
2024-02-05 20:53:15 +0100

    Program finished

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from mpmath import *
 #setting number of digits
 mp.dps = 1000
 
-
-
+#setting up pi and the digit of pi you want
 pi = str(mp.pi)
-dig = int(input('Instert the digit from pi that you want: '))
+dec = int(input('Instert the decimal of pi you want: '))
+realdec = dec + 2
+pidec = pi[0:realdec]
+
+print(pidec)
+
 
